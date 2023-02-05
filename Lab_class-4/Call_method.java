ee500d0f43db070995cd6514072d168cb1ecf5a9
Md Bappy <mdbappy21.cse@gmail.com>
2023-02-06 02:47:39 +0600

    Update Call_method.java

--- Lab_class-4/Call_method.java
+++ Lab_class-4/Call_method.java
@@ -1,1 +1,16 @@
-
+public class Call_method {
+    //Static method.
+    static void s_method(){
+        System.out.println("Static method");
+    }
+    //Public method.
+    public void method(){
+        System.out.println("Non-static method");
+    }
+    //Main method
+    public static void main(String[] args) {
+        s_method();
+        Call_method obj = new Call_method();
+        obj.method();
+    }
+}
