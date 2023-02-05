d2e261f141588fe0ac48c8764adb8f5f96e41d54
Md Bappy <mdbappy21.cse@gmail.com>
2023-02-06 02:50:54 +0600

    LabClass-4

--- Lab_class-4/Call_method.java
+++ Lab_class-4/Call_method.java
@@ -9,8 +9,10 @@ public class Call_method {
     }
     //Main method
     public static void main(String[] args) {
+       //static method call.
         s_method();
         Call_method obj = new Call_method();
+        //Non-static method call.
         obj.method();
     }
 }
