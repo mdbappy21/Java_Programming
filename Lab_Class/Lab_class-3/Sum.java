bbf93eb79c67928507d6d8a5b7dc508cbef305fc
Md Bappy <mdbappy21.cse@gmail.com>
2023-02-08 15:31:25 +0600

    Lab class-3

---
+++ Lab_Class/Lab_class-3/Sum.java
@@ -0,0 +1,16 @@
+//Calculate sum in a method and display.
+public class Sum {
+    void Add(int n){
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            sum+=i;
+        }
+        System.out.println(sum);
+    }
+    public static void main(String[] args){
+      int num=10;
+      Sum obj=new Sum();
+      obj.Add(num);
+
+    }
+}
