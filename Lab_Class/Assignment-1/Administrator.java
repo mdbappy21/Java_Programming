a489549ff8cc492e72d3341d2fcd8a0fc2ed3639
Md Bappy <123102557+mdbappycse@users.noreply.github.com>
2023-04-22 21:11:53 +0600

    Admin

---
+++ Lab_Class/Assignment-1/Administrator.java
@@ -0,0 +1,16 @@
+public class Administrator extends Employee {
+    private String Department;
+
+    public Administrator(int EmployeeId, String Name, double Salary, String Department) {
+        super(EmployeeId,Name, Salary);
+        this.Department = Department;
+    }
+
+    public String getDepartment() {
+        return Department="Computer Science and Engineering";
+    }
+
+    public String toString() {
+        return super.toString() + "\nDepartment: " + Department;
+    }
+}
