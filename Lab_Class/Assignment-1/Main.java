31cbca7bcd3df70fd4d376c993b5192bb5d458a0
Md Bappy <123102557+mdbappycse@users.noreply.github.com>
2023-04-23 10:15:45 +0600

    Main Class

--- Lab_Class/Assignment-1/Main.java
+++ Lab_Class/Assignment-1/Main.java
@@ -1,24 +1,36 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+
+        ArrayList<Employee>Employees =new ArrayList<>();
 
         System.out.println("Professor's Information-1 : ");
         Professor professor1 = new Professor(1000, "Akash", 60000, "Object Oriented Programming");
         System.out.println(professor1.toString()+"\n");
 
+
         System.out.println("Professor's Information-2 : ");
         Professor professor2=new Professor(1001,"Auntu",60000,"Programming and Problem Solving");
         System.out.println(professor2.toString()+"\n");
 
+
         System.out.println("Support Staff's Information : ");
-        SupportStaff supportStaff1 = new SupportStaff(2000, "Pranto", 40000, "Assistant");
+        SupportStaff supportStaff1 = new SupportStaff(2000, "Pranto", 30000, "Assistant");
         System.out.println(supportStaff1.toString()+"\n");
+
 
         System.out.println("Administrator's Information : ");
         Administrator Administrator1 = new Administrator(3000, "Bappy", 50000, "Computer Science and Engineering");
         System.out.println(Administrator1.toString()+"\n");
 
-        Employee[] employees = { professor1,professor2, Administrator1, supportStaff1 };
+        //Adding All Info in Employees ArrayList.
+        Employees.add(professor1);
+        Employees.add(professor2);
+        Employees.add(supportStaff1);
+        Employees.add(Administrator1);
 
-        System.out.println("Total Salary: $" + Employee.totalSalary(employees));
+        //Printing Total Salary of Employees.
+        System.out.println("Total Salary : " + Employee.totalSalary(Employees)+" BDT");
     }
 }
